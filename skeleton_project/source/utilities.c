093e77eda7424e02ec8ac11dfb10d2150a06c9a7
Kasper Qvale-Seterelv <kasper.qvale@gmail.com>
2026-02-18 11:37:13 +0100

    laget en start på en sirkulær kø siden jeg kjeda meg litt i forelesning

---
+++ skeleton_project/source/utilities.c
@@ -0,0 +1,31 @@
+#include "utilities.h"
+
+void init_queue(Queue *q){
+    q->head = -1;
+    q->tail = -1;
+}
+
+bool queue_is_full(Queue *q){
+    return (q->head == (q->tail + 1) % MAX_SIZE);
+}
+
+bool queue_is_empty(Queue *q){
+    return (q->head == -1);
+}
+
+void add_to_queue(Queue *q, Order order){
+    if (queue_is_full(q)){
+        //gjør no greier her kall en interupt eller no dette skal ikke skje
+    }
+    q->tail =  (q->tail + 1) % MAX_SIZE;
+    q->arr[q->tail] = order;
+}
+
+Order pop_queue(Queue *q){
+    if(queue_is_empty(q)){
+        //Dette kan skje det må vi håndtere kanskjen returnere nada.
+    }
+    Order current_order = q->arr[q->head];
+    q->head = (q->head + 1) % MAX_SIZE;
+    return current_order;
+}
